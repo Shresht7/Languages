4aef43921b5b10dedfbae44a1f9615b4e725cd76
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-02 14:54:25 +0530

    Update language colors

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -1,6 +1,11 @@
 const colors = {
-    TypeScript: '#0000FF',
-    Go: '#33CCFF',
+    TypeScript: '#2b7489',
+    JavaScript: '#f1e05a',
+    HTML: '#e34c26',
+    CSS: '#563d7c',
+    Go: '#00ADD8',
+    Rust: '#dea584',
+    Svelte: '#ff3e00'
 }
 
 /** Returns the color corresponding to the given language */
