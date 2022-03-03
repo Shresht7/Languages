0311ff98837140a8633628ec015e7473b7cc5167
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-03 23:05:55 +0530

    Refactor colors.js script

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,17 +1,20 @@
-// * This is a Deno script. Run using `deno run --allow-net --allow-write scripts/github.js`
-//  TODO: Move this into a GitHub Action
+//  Library
+import * as fs from 'node:fs'
+import { parse } from 'js-yaml'
+import fetch from 'node-fetch'
 
-import { parse, stringify } from 'https://deno.land/std@0.127.0/encoding/yaml.ts'
-
+//  Get github/linguist's language.yml file
 const data = await fetch('https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml')
     .then(res => res.text())
 
-
+//  Parse yaml file
 const languages = parse(data)
 
+//  Generate language-color map
 const colors = {}
 for (const [language, { color }] of Object.entries(languages)) {
     colors[language] = color
 }
 
-Deno.writeTextFile("./src/data/colors.json", JSON.stringify(colors, null, 2))
+//  Write file to disk
+fs.writeFileSync('./src/data/colors.json', JSON.stringify(colors, null, 2))
