c6462030ea37e56da48967ca928015d689607285
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-03 10:35:33 +0530

    Style Input Component

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -4,10 +4,42 @@
 
 <div>
     <form on:submit|preventDefault>
-        <input bind:value />
+        <input bind:value placeholder="username" />
         <button type="submit">Get</button>
     </form>
 </div>
 
 <style>
+    div,
+    form {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+
+    form {
+        gap: 0.75rem;
+    }
+
+    input,
+    button {
+        font: inherit;
+        padding: 0.5rem 1rem;
+    }
+
+    button {
+        color: var(--clr-background);
+        background-color: var(--clr-text);
+        border: 1px solid var(--clr-text);
+        border-radius: 4px;
+        transition: all var(--animation-duration) ease-in;
+        cursor: pointer;
+    }
+
+    button:hover,
+    button:focus {
+        opacity: 0.8;
+    }
 </style>
