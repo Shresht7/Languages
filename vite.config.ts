200bde1d8df3ad7af9a8a90b603f97d4aaa24f29
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-03 12:05:53 +0530

    Use build folder instead of dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Languages/',
+  build: {
+    outDir: './build/'
+  },
   plugins: [svelte()],
 })
