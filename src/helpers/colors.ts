1b953f1bc892c0d90f0cdaa725c17cdbe9009695
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-03 17:36:09 +0530

    Support language colors Fixes #6

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -1,12 +1,5 @@
-const colors = {
-    TypeScript: '#2b7489',
-    JavaScript: '#f1e05a',
-    HTML: '#e34c26',
-    CSS: '#563d7c',
-    Go: '#00ADD8',
-    Rust: '#dea584',
-    Svelte: '#ff3e00'
-}
+//  Data
+import colors from '../data/colors.json'
 
 /** Returns the color corresponding to the given language */
 export const getLanguageColor = (language: string): string => colors[language] ?? 'black'
