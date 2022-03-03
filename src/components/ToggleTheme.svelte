d3feffe093730de8b5eb8c26e346431e0f5d1dfb
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-03 16:08:32 +0530

    Add toggle theme button

---
+++ src/components/ToggleTheme.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    //  Components
+    import Button from "../components/utility/Button.svelte";
+    import Icon from "../components/utility/Icon.svelte";
+    import IoIosSun from "svelte-icons/io/IoIosSunny.svelte";
+    import IoIosMoon from "svelte-icons/io/IoIosMoon.svelte";
+
+    //  Themes
+    import theme from "../themes/store";
+    import type { themeMode } from "src/types";
+
+    let mode: themeMode = "light";
+    $: mode = $theme ? theme.getCurrentTheme() : "light";
+</script>
+
+<Button on:click={() => theme.toggle()} style="padding: 0.5rem;">
+    {#if mode === "light"}
+        <Icon>
+            <IoIosMoon />
+        </Icon>
+    {:else}
+        <Icon>
+            <IoIosSun />
+        </Icon>
+    {/if}
+</Button>
